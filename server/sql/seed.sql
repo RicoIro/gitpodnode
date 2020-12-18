7b26943f28d5626e782454220564de25e87c08fa
cam-emmauscollege <cam@emmauscollege.nl>
2020-12-18 09:44:47 +0000

    update in readme en tasks

--- server/sql/seed.sql
+++ server/sql/seed.sql
@@ -1,2 +1,2 @@
-INSERT INTO baanStatus (status, tijd, opmerking) VALUES (1, CURRENT_TIMESTAMP, "Server is aan")
+INSERT INTO baanStatus (status, tijd, opmerking) VALUES (1, CURRENT_TIMESTAMP, "Server is aan");
 INSERT INTO buttonPresses (tijd) VALUES (CURRENT_TIMESTAMP);
